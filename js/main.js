09d87d24ddd59295f50873ea3f1a63615b874dea
何jw <846255299@qq.com>
2022-07-19 17:18:39 +0800

    feat:加上随机笑话生成器

--- js/main.js
+++ js/main.js
@@ -1,22 +1,45 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
 
-let myHeading = document.querySelector('h1');
-myHeading.onclick = setUserName;
+console.log(customName)
+console.log(randomize)
+console.log(story)
 
-setTitle()
-
-function setUserName() {
-    let myName = prompt('请输入你的名字。');
-    console.log("myName:" + myName)
-    if (!myName) {
-        console.log("myName is not null")
-    } else {
-        console.log("myName is null")
-    }
-    localStorage.setItem('name', myName);
-    setTitle()
+function randomValueFromArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
 }
 
-function setTitle() {
-    let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+let storyText = "今天气温 34 摄氏度，:inserta:出去遛弯。当走到:insertb:门前时，突然就:insertc:。人们都惊呆了，李雷全程目睹但并没有慌，因为:inserta:是一个 130 公斤的胖子，天气又辣么热。"
+let insertX = ['怪兽威利', '大老爹', '圣诞老人']
+let insertY = ['肯德基', '迪士尼乐园', '白宫']
+let insertZ = ['自燃了', '在人行道化成了一坨泥', '变成一条鼻涕虫爬走了']
+
+randomize.addEventListener('click', result);
+
+function result() {
+    let newStory = storyText
+    let xItem = randomValueFromArray(insertX)
+    let yItem = randomValueFromArray(insertY)
+    let zItem = randomValueFromArray(insertZ)
+
+    newStory = newStory.replace(':inserta:', xItem);
+    newStory = newStory.replace(':inserta:', xItem);
+    newStory = newStory.replace(':insertb:', yItem);
+    newStory = newStory.replace(':insertc:', zItem);
+
+    if (customName.value !== '') {
+        let name = customName.value;
+        newStory = newStory.replace('李雷', name);
+    }
+
+    if (document.getElementById("american").checked) {
+        let weight = Math.round(300);
+        let temperature = Math.round(94);
+        newStory = newStory.replace('34 摄氏度', temperature);
+        newStory = newStory.replace('130 公斤', weight);
+    }
+
+    story.textContent = newStory;
+    story.style.visibility = 'visible';
 }
